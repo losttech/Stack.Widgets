48d9e9e6249f424ffa8c084cb20841ec9fcdd818
Victor Milovanov <lost@losttech.software>
2018-05-02 22:54:56 -0700

    used WindowsFormHost hack to make browser control work on a window with AllowsTransparency enabled

---
+++ src/Helpers/TransparentWebBrowser.cs
@@ -0,0 +1,56 @@
+﻿namespace LostTech.Stack.Widgets.Helpers
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public class TransparentWebBrowser: Control
+    {
+        WebBrowserOverlayWindow _WebBrowserOverlayWindow;
+        public static readonly DependencyProperty TargetElementProperty =
+            DependencyProperty.Register(nameof(TargetElement), typeof(FrameworkElement), typeof(TransparentWebBrowser),
+                new PropertyMetadata(TargetElementPropertyChanged));
+        public FrameworkElement TargetElement {
+            get => this.GetValue(TargetElementProperty) as FrameworkElement;
+            set => this.SetValue(TargetElementProperty, value);
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register(nameof(Source), typeof(string), typeof(TransparentWebBrowser),
+                new PropertyMetadata(SourcePropertyChanged));
+        public string Source {
+            get => this.GetValue(SourceProperty) as string;
+            set => this.SetValue(SourceProperty, value);
+        }
+
+        private static void SourcePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args) {
+            if (sender is TransparentWebBrowser transparentWebBrowser) {
+                transparentWebBrowser._WebBrowserOverlayWindow.Source = args.NewValue as string;
+            }
+        }
+
+        private static void TargetElementPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args) {
+            if (sender is TransparentWebBrowser transparentWebBrowser) {
+                transparentWebBrowser._WebBrowserOverlayWindow.TargetElement = args.NewValue as FrameworkElement;
+            }
+        }
+
+        public TransparentWebBrowser() {
+            this._WebBrowserOverlayWindow = new WebBrowserOverlayWindow();
+
+            //TODO: Figure out how to automatically set the TargetElement binding...
+
+            //var targetElementBinding = new Binding();
+            //var rs = new RelativeSource();
+            //rs.AncestorType = typeof(Border);
+            //targetElementBinding.RelativeSource = rs;
+
+            //_WebBrowserOverlayWindow.SetBinding(TransparentWebBrowser.TargetElementProperty, targetElementBinding);
+
+
+        }
+
+        static TransparentWebBrowser() {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TransparentWebBrowser), new FrameworkPropertyMetadata(typeof(TransparentWebBrowser)));
+        }
+    }
+}
--- src/WebView.xaml.cs
+++ src/WebView.xaml.cs
@@ -1,28 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Widgets
+﻿namespace LostTech.Stack.Widgets
 {
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Navigation;
+
     /// <summary>
     /// Interaction logic for WebView.xaml
     /// </summary>
     public partial class WebView : UserControl
     {
-        public WebView()
-        {
-            InitializeComponent();
+        public WebView() {
+            this.InitializeComponent();
+
+            //this.Browser.Navigated += this.BrowserOnNavigated;
         }
+
+        void BrowserOnNavigated(object sender, NavigationEventArgs e) {
+            if (this.URL?.Equals(e.Uri) == true)
+                return;
+
+            this.URL = e.Uri;
+        }
+
+        #region URL
+        public Uri URL {
+            get { return (Uri)this.GetValue(URLProperty); }
+            set { this.SetValue(URLProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty URLProperty =
+            DependencyProperty.Register(nameof(URL), typeof(Uri), typeof(WebView), 
+                new PropertyMetadata(defaultValue: new Uri("about:blank")
+                    //, propertyChangedCallback: UrlChanged
+                    ),
+                validateValueCallback: ValidateUrl);
+
+        static bool ValidateUrl(object value) {
+            if (value is Uri uri) {
+                string scheme = uri.Scheme?.ToLowerInvariant();
+                return scheme == "http" || scheme == "https"
+                    || scheme == "about" && uri.AbsoluteUri == "about:blank";
+            }
+
+            return false;
+        }
+
+        //static void UrlChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        //    var self = (WebView)d;
+        //    var destination = e.NewValue as Uri ?? new Uri("about:blank");
+        //    self.Browser.Navigate(destination);
+        //}
+        #endregion URL
     }
 }
