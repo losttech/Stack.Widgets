d6eae9966fb2d30adf600a5565f9133d8e5321f1
Victor Milovanov <lost@losttech.software>
2018-06-14 14:58:15 -0700

    added RefreshableDataSource

---
+++ src/DataSources/RefreshableDataSource.cs
@@ -0,0 +1,28 @@
+﻿namespace LostTech.Stack.Widgets.DataSources
+{
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Input;
+    using LostTech.Stack.Widgets.DataBinding;
+    using Prism.Commands;
+
+    public class RefreshableDataSource: DependencyObjectNotifyBase, IRefreshable {
+        public object Source {
+            get => GetValue(SourceProperty);
+            set => SetValue(SourceProperty, value);
+        }
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register(nameof(Source), typeof(object), typeof(RefreshableDataSource), new PropertyMetadata(null));
+
+        public RefreshableDataSource() {
+            this.RefreshCommand = new DelegateCommand(this.Refresh);
+        }
+
+        public ICommand RefreshCommand { get; }
+
+        void Refresh() {
+            var bindingExpression = BindingOperations.GetBindingExpression(this, SourceProperty);
+            bindingExpression?.UpdateTarget();
+        }
+    }
+}
