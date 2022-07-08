9ddb5baf840eeb52488259a5f3a76387cdf9e675
Victor Milovanov <lost@losttech.software>
2022-07-08 12:42:59 -0700

    two new converters: Formatter and ConverterPipeline

---
+++ src/DataBinding/ConverterPipeline.cs
@@ -0,0 +1,28 @@
+﻿namespace LostTech.Stack.Widgets.DataBinding;
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+public class ConverterPipeline : List<ParametrizedConverter>, IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => this.Aggregate(value, (current, converter) => converter.Converter.Convert(current, targetType, converter.Parameter, culture));
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => this.Reversed()
+               .Aggregate(value, (current, converter) => converter.Converter.ConvertBack(current, targetType, converter.Parameter, culture));
+
+    IEnumerable<ParametrizedConverter> Reversed() {
+        for (int i = this.Count - 1; i >= 0; i--)
+            yield return this[i];
+    }
+}
+
+[ContentProperty(nameof(Converter))]
+public sealed class ParametrizedConverter {
+    public IValueConverter Converter { get; set; }
+    public object Parameter { get; set; }
+}
---
+++ src/DataBinding/Formatter.cs
@@ -0,0 +1,18 @@
+﻿namespace LostTech.Stack.Widgets.DataBinding;
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+public class Formatter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        if (parameter is not string format)
+            throw new ArgumentException("Parameter must be a format string");
+        return string.Format(format, value);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
