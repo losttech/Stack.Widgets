ca715fa93e702bff48c7cb1c76ed110bf08623f6
Victor Nova <lost@losttech.software>
2022-08-24 10:44:39 -0700

    fixed memory leaks with refreshing data sources upon layout unloading

--- src/DataSources/DataSource.cs
+++ src/DataSources/DataSource.cs
@@ -1,6 +1,7 @@
 ﻿namespace LostTech.Stack.Widgets.DataSources
 {
     using System;
+    using System.Diagnostics;
     using System.Windows;
     using System.Windows.Threading;
 
@@ -18,39 +19,37 @@
 
         static readonly DependencyPropertyKey RefresherPropertyKey =
             DependencyProperty.RegisterAttachedReadOnly("__Refresher__",
-                typeof(DispatcherTimer), typeof(DependencyObject), new PropertyMetadata(null));
+                typeof(Refresher), typeof(DependencyObject), new PropertyMetadata(null));
 
         static void OnRefreshIntervalChanged(DependencyObject d, DependencyPropertyChangedEventArgs change) {
             if (d is not IRefreshable refreshable)
                 return;
 
-            var timer = (DispatcherTimer)d.GetValue(RefresherPropertyKey.DependencyProperty);
+            var refresher = (Refresher)d.GetValue(RefresherPropertyKey.DependencyProperty);
             Duration value = change.NewValue is Duration duration ? duration : Duration.Forever;
 
             if (value.HasTimeSpan) {
-                if (timer == null) {
-                    timer = new DispatcherTimer {
-                        Tag = refreshable,
-                    };
-                    timer.Tick += RefreshTimerOnTick;
-                    d.SetValue(RefresherPropertyKey, timer);
-                    // very inefficient, assumes infrequent changes to RefreshInterval
-                    Application.Current.Dispatcher.ShutdownStarted += delegate { timer?.Stop(); };
+                if (refresher == null) {
+                    refresher = new Refresher(refreshable);
+                    refresher.Timer.Tick += RefreshTimerOnTick;
+                    d.SetValue(RefresherPropertyKey, refresher);
                 }
 
-                timer.Interval = value.TimeSpan;
-                timer.IsEnabled = true;
-            } else if (timer != null) {
-                timer.Stop();
+                refresher.Timer.Interval = value.TimeSpan;
+                refresher.Timer.IsEnabled = true;
+            } else if (refresher != null) {
+                refresher.Dispose();
                 d.SetValue(RefresherPropertyKey.DependencyProperty, null);
             }
         }
 
         static void RefreshTimerOnTick(object? sender, EventArgs e) {
-            var timer = (DispatcherTimer)sender!;
-            var refreshable = (IRefreshable)timer.Tag;
-            if (refreshable.RefreshCommand.CanExecute(null))
+            var refresher = (Refresher)((DispatcherTimer)sender!).Tag;
+            var refreshable = refresher.Refreshable;
+            if (refreshable.RefreshCommand.CanExecute(null)) {
+                Trace.WriteLine($"refreshing {refreshable}");
                 refreshable.RefreshCommand.Execute(null);
+            }
         }
     }
 }
---
+++ src/DataSources/Refresher.cs
@@ -0,0 +1,23 @@
+﻿namespace LostTech.Stack.Widgets.DataSources;
+
+using System;
+using System.Windows.Threading;
+
+class Refresher : IDisposable {
+    readonly EventHandler weakDispose;
+    public DispatcherTimer Timer { get; }
+    public IRefreshable Refreshable { get; }
+
+    public Refresher(IRefreshable refreshable) {
+        this.Refreshable = refreshable;
+        this.Timer = new DispatcherTimer { Tag = this };
+        var self = new WeakReference(this);
+        this.weakDispose = (_, __) => (self.Target as IDisposable)?.Dispose();
+        this.Timer.Dispatcher.ShutdownStarted += this.weakDispose;
+    }
+
+    public void Dispose() {
+        this.Timer.Dispatcher.ShutdownStarted -= this.weakDispose;
+        this.Timer.Stop();
+    }
+}
